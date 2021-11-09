89be0acdd9b2167298ec0b2903d8d68fb9a7b279
Sander Mendes <sandermendes@gmail.com>
2021-11-09 19:13:30 -0300

    Implemented conditional database connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,15 @@
 import Sequelize from 'sequelize'
 
-export const sequelize = new Sequelize({
+export const sequelize = (process.env.DB_DIALECT === 'sqlite') ? ( new Sequelize({
     dialect: process.env.DB_DIALECT,
     storage: process.env.DB_PATH_STORAGE
-})
+})) : (process.env.DB_DIALECT === 'mariadb') && (
+    new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    })
+)
 
 // const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
 //     dialect: process.env.DB_DIALECT,
