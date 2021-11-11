d6838adb084fdc007876042d0396305f864fc806
Sander Mendes <sandermendes@gmail.com>
2021-11-10 21:05:47 -0300

    Disable sequelize console log

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,12 +2,14 @@ import Sequelize from 'sequelize'
 
 export const sequelize = (process.env.DB_DIALECT === 'sqlite') ? ( new Sequelize({
     dialect: process.env.DB_DIALECT,
-    storage: process.env.DB_PATH_STORAGE
+    storage: process.env.DB_PATH_STORAGE,
+    logging: false
 })) : (process.env.DB_DIALECT === 'mariadb') && (
     new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
         dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
+        logging: false
     })
 )
 
