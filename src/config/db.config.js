ffdf35f0215acd6852dd793caa9690856ff12e61
Sander Mendes <sandermendes@gmail.com>
2021-11-16 20:14:21 -0300

    Update dialect to mysql

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,7 +4,7 @@ export const sequelize = (process.env.DB_DIALECT === 'sqlite') ? ( new Sequelize
     dialect: process.env.DB_DIALECT,
     storage: process.env.DB_PATH_STORAGE,
     logging: false
-})) : (process.env.DB_DIALECT === 'mariadb') && (
+})) : (process.env.DB_DIALECT === 'mysql') && (
     new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
         dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
