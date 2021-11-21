a46b3eaebd22a9338f4125ae9562b63728060317
Sander Mendes <sandermendes@gmail.com>
2021-11-21 16:25:34 -0300

    Added file, right way to setup sequelize in Docker, with env variables.

---
+++ data-migration/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+        "username": "root",
+        "password": "123456",
+        "database": "database_development",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": process.env.DB_DIALECT
+    }
+}
